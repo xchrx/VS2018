1d2c4fa08ea917535ce6c21e93273b2a4fc62ae2
Christian <Christianjuenger93@gmail.com>
2018-06-16 09:20:07 +0200

    Uebung 5

---
+++ Uebung5/src/uebung5/Bericht.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uebung5;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Chris
+ */
+public class Bericht implements Serializable{
+    Date datum;
+    String diagnose;
+    String weiteresVorgehen;
+
+    public Bericht() {
+    }
+
+    public Bericht(Date datum, String diagnose, String weiteresVorgehen) {
+        this.datum = datum;
+        this.diagnose = diagnose;
+        this.weiteresVorgehen = weiteresVorgehen;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public void setDatum(Date datum) {
+        this.datum = datum;
+    }
+
+    public String getDiagnose() {
+        return diagnose;
+    }
+
+    public void setDiagnose(String diagnose) {
+        this.diagnose = diagnose;
+    }
+
+    public String getWeiteresVorgehen() {
+        return weiteresVorgehen;
+    }
+
+    public void setWeiteresVorgehen(String weiteresVorgehen) {
+        this.weiteresVorgehen = weiteresVorgehen;
+    }
+    
+    
+}
---
+++ Uebung5/src/uebung5/Röntgenbild.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uebung5;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Chris
+ */
+public class Röntgenbild implements Serializable{
+
+    Date aufnahmeVom;
+    transient String  patientenName;
+    
+    public Röntgenbild()
+    {
+        
+    }
+
+    public Röntgenbild(Date aufnahmeVom, String patientenName) {
+        this.aufnahmeVom = aufnahmeVom;
+        this.patientenName = patientenName;
+    }
+ 
+    public Date getAufnahmeVom() {
+        return aufnahmeVom;
+    }
+
+    public void setAufnahmeVom(Date aufnahmeVom) {
+        this.aufnahmeVom = aufnahmeVom;
+    }
+
+    public String getPatientenName() {
+        return patientenName;
+    }
+
+    public void setPatientenName(String patientenName) {
+        this.patientenName = patientenName;
+    }
+    
+    
+}
